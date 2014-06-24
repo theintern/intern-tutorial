abee3b66ae0263c0388df3e91eadd90c6325f6b2
Jason Cheatham <j.cheatham@gmail.com>
2014-06-24 17:02:43 -0400

    Update tests and config for Intern 2.0.0

--- tests/functional/index.js
+++ tests/functional/index.js
@@ -9,16 +9,17 @@ define([
 		'greeting form': function () {
 			return this.remote
 				.get(require.toUrl('index.html'))
-				.waitForElementByCssSelector('body.loaded', 5000)
-				.elementById('nameField')
+				.setFindTimeout(5000)
+				.findByCssSelector('body.loaded')
+				.findById('nameField')
 					.clickElement()
 					.type('Elaine')
 					.end()
-				.elementByCssSelector('#loginForm input[type=submit]')
+				.findByCssSelector('#loginForm input[type=submit]')
 					.clickElement()
 					.end()
-				.elementById('greeting')
-				.text()
+				.findById('greeting')
+				.getVisibleText()
 				.then(function (text) {
 					assert.strictEqual(text, 'Hello, Elaine!', 'Greeting should be displayed when the form is submitted');
 				});
